d124591357e7cf45a2d235a59fe9bbfd68bc10ec
jorge-imperial <jorgeluis.imperial@gmail.com>
2020-10-02 06:29:24 -0700

    Sample configuration

---
+++ src/config.js
@@ -0,0 +1,16 @@
+// The Realm App ID from MongoDB.
+exports.realmAppId = '';
+
+// User and password (using email authentication)
+exports.realm_user = '';
+exports.realm_pwd  = '';
+
+// How often the status is updated
+exports.status_timeout = 30 * 1000;
+// Host and port where the daemon.py is running. This would be ideally localhost
+exports.daemon_host = '192.168.100.254';
+exports.daemon_port = 9000;
+
+// Sprinkler ID, 
+exports.controller_id  = '00000000';
+
