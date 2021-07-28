63b533fb882bc2d50e70f653a79ef1133b21ef66
jorge.imperial <jorgeluis.imperial@gmail.com>
2021-07-28 10:35:35 -0700

    Syncing data - not tryed Relays embedded yet

--- src/index.js
+++ src/index.js
@@ -1,150 +1,93 @@
-const Realm  = require("realm");
-const config = require("./config");
 const schemas = require("./schemas");
- 
-const http = require('http');
+const users = require("./users");
+const config = require("./config");
 
+const http = require("http");
+const BSON = require("bson");
+const Realm = require("realm")
 
-const STATUS_TIMEOUT = config.status_timeout;
-const HOST = config.daemon_host;
-const PORT = config.daemon_port;
-const CONTROLLER_ID = config.controller_id;
-const REALM_APP_ID = config.realmAppId;
-const USER = config.realm_user;
-const PWD = config.realm_pwd;
+let realm = 0;
 
-const appConfig = {
-    id: REALM_APP_ID,
-    timeout: 10000,
-};
-const app = new Realm.App(appConfig);
-
- 
-async function  getSprinklerStatus(realm) {
-
+async function  getSprinklerStatus() {
     const options = {
-        hostname: HOST,
-        port: PORT,
+        hostname: config.daemon_host,
+        port: config.daemon_port,
         path: '/status',
         method: 'GET'
-      }
+    }
 
     const req = http.request(options, res => {
         console.log(`statusCode: ${res.statusCode}`)
-    
+
         res.on('data', d => {
-            process.stdout.write(d);
-            console.log(d.toString());
-             // Upsert
+
             try {
                 const timeNow = Date.now();
-                realm.write( () => {
+                console.log(`Got status: ${d.toString()}`);
 
-                    const newStatus = realm.create( "Status", {
-                        _id: CONTROLLER_ID + '-' + timeNow,
-                        _partition: CONTROLLER_ID,
-                        relays:  d.toString(),      // 
-                        timestamp: timeNow,         // 
-                        status: 0                   //  
-                    }, 
-                    "modified"); 
-                    console.log('Wrote status at ' + timeNow);
-                    req.end();
-
+                const obj = JSON.parse(d.toString());
+                const relay_array = [];
+                obj.forEach((v) => {
+                    // {"gpio":2,"name":"sprinkler0","state":false,"use":"valve"}
+                    relay_array.push({
+                        gpio: v.gpio,
+                        name: v.name,
+                        state: (v.state) ? 1 : 0,
+                        use: v.use
+                    });
                 });
-            }
-            catch (e) {
-                console.error('Could not write status data from sprinklers.');
+
+                realm.write(() => {
+                    const current_status = realm.create("Status", {
+                        _partition: config.controller_id,
+                        timestamp: timeNow,
+                        //relays: relay_array,
+                        _id: new BSON.ObjectID,
+                        status: 0
+                    });
+                });
+ 
+            } catch (e) {
+                console.error('Could not send/parse data from sprinklers.');
                 console.error(e);
             }
         })
     })
 
     req.on('error', error => {
+        console.error('Could not send/parse data from sprinklers.');
         console.error(error)
-      })
-      
-    req.end()
-}
- 
+    })
 
-
-
-
-async function  sendCommandToSprinkler(command) {
-
-    /*
-    const sprinklers = net.connect({port: PORT, host: HOST},
-        function () {
-             console.log('Connected to sprinklers!')
-        });
-    sprinklers.write(command);
-    sprinklers.on('data', function(data) {
-        console.log(data.toString());
-        sprinklers.end();
-    });
-    sprinklers.on('end', function() {
-        console.log('Disconnected from sprinklers.')
-    });
-    */
+    req.end();
 }
 
+async function run() {
+    await users.logIn();
 
-async function logIn(email, password) {
+    const user = await users.getAuthedUser();
 
-    try {
-        console.log('Authenticating')
-        const credentials = Realm.Credentials.emailPassword(email, password);
-        const user = await app.logIn(credentials);
-        if (user) {
-            console.log("You have successfully logged in as " + app.currentUser.id);
-            return 0;
-        } else {
-            console.error("There was an error logging you in");
-            return 1;
-        }
-    } catch (err) {
-        console.error(JSON.stringify(err, null, 2));
-        return 2;
-    }
-}
-
-
-function listener(commands, changes) {
-
-    // Only care about newly inserted commands
-    changes.insertions.forEach((index) => {
-        let insertedCmd = commands[index];
-        const t = insertedCmd.timestamp
-        const c = insertedCmd.command
-
-        sendCommandToSprinkler(insertedCmd.command)
-
-        console.log(c)
-    });
-}
-
-
-async function  run() {
-
-    await logIn(USER, PWD);
-    const realm = await Realm.open({
-        schema: [schemas.StatusSchema, schemas.CommandsSchema],
+    const Realm_Config =  {
+        schema: [schemas.StatusSchema],
         sync: {
-            user: app.currentUser,
-            partitionValue: CONTROLLER_ID,
+            user: user,
+            partitionValue: config.controller_id,
         },
-    });
+    };
 
-    setInterval(getSprinklerStatus, STATUS_TIMEOUT, realm);
+    realm = await Realm.open(Realm_Config);
+
+    setInterval(getSprinklerStatus, config.status_timeout, Realm);
 
     // New commands?
     //const commands = realm.objects("Commands");
     //commands.addListener(listener);
-
 }
 
 
-run().catch(err => {
-    console.error("Failed to open realm:", err)
+
+
+
+run().catch((err) => {
+    console.error(err.message);
 });
